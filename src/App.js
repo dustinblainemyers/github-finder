e033d4bf25049b813bda229209fa442474b42441
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-28 14:52:23 -0400

    creating navbar class components - section 2 component props and types

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
+import Navbar from './components/layout/Navbar';
 
 import "./App.css";
 
 class App extends React.Component {
   foo = () => "bars";
   render() {
-    const name = "John Doe";
-    const loading = false;
-    const showName = true;
+   
 
     return (
-      <div className='App'>
-        {loading ? <h4> Loading </h4>: <h1>
-          Hello {showName && name} {this.foo}
-        </h1>}
+      
+      <nav className="navbar bg-primary">
+        <Navbar/>
+      </nav>
+       
         
-      </div>);
+      
+        );
     
   }
 }
