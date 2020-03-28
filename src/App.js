d9b83318adfa7cf145489689626552f929dd59fd
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-27 20:47:42 -0400

    halfway through section 2 part 9

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
 
+  foo = () => 'bars'
   render() {
+    const name = 'John Doe';
+    const loading = false;
 
+    if(loading) {
+      return <h4> Loading...</h4>
+    }
+    
     return (
-      <div className="App">
-        <h1>Hello from React</h1>
+      <div className='App'>
+        <h1>Hello {name} {this.foo}</h1>
       </div>
     );
-
   }
-  
 }
 
 export default App;
